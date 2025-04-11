79e29aedc14f51ec492cd1b43536ed3088719fad
= <akkv.2507@gmail.com>
2025-04-11 14:40:47 +0530

    fix(rabbitmq): switch from localhost to 127.0.0.1 to prevent IPv6 connection issue

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,16 @@ import (
 	"log"
 
 	"github.com/AthulKrishna2501/zyra-msg-service/internals/broker"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
 	log.Println("Starting Message Service...")
 	broker.InitRabbitMQ()
 
